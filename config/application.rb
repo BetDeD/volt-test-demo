bec0f83aa6053584837f4810643180ae1dcd791f
betded <kontorka@list.ru>
2019-02-08 12:45:57 +0300

    Heroku bug fix

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,10 @@ module VoltTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.assets.compile = false
     config.assets.initialize_on_precompile = false
-    
+
     config.autoload_paths << Rails.root.join('lib')
 
     config.api_only = true
